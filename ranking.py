0a2e08b271147a6fbecd8205a613e44514eed9ed
Celeste Elizondo <m.elizondo.6@estudiantec.cr>
2026-06-24 19:37:40 -0600

    Agregando funciones faltantes

--- ranking.py
+++ ranking.py
@@ -10,12 +10,23 @@ def cargar_usuarios():
         return {}
     with open(ARCHIVO_USUARIOS, "r") as f: # cuando se abre el archivo saca los datos de los jugadores y los deja listos para utilizarlos.
         return json.load(f)
+
 #busca los usuarios 
 def top_defensores():
     usuarios = cargar_usuarios()
     ordenados = sorted( # se acomodan de mayor a menor tomando en cuenta solo la cantidad de victorias 
         usuarios.items(),
-        key=lambda x: x[1]["victorias_defensor"], #busca el dato victorias
+        key=lambda x: x[1]["victorias_defensor"], #busca el dato victorias como defensor
         reverse=True
     )
     return ordenados#la lista no se corta para que retorne todos los puntajes
+
+#aqui se retonan todos los jugadores ordenados por victorias siendo atacantes
+def top_atacantes():
+    usuarios = cargar_usuarios()
+    ordenados = sorted(
+        usuarios.items(),
+        key=lambda x: x[1]["victorias_atacante"], # busca el dato victorias_atacante
+        reverse=True
+    )
+    return ordenados # la lista no se corta para que retorne todos los puntajes
